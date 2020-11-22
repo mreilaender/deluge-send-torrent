75e8b7ed64274d8771bb235acaa7ac1cdefbc508
Manuel Reiländer <manuel.reilaender@sonorys.at>
2020-11-22 13:54:46 +0100

    use setuptools to install this as a CLI

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='send-to-deluge',
+    version='1.0',
+    scripts=['./scripts/send-to-deluge'],
+    author='Manuel Reilaender',
+    description='Sends torrent files to deluge only if certain criteria are met',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=[
+        'setuptools',
+        'bencodepy == 0.9.5'
+    ],
+    python_requires='>=3.8'
+)
